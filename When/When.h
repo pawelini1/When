c9794bcf2122204a1dba2ecd8c69dd67449d3103
Paweł Szymański <pawel.szymanski@grandparade.co.uk>
2020-03-13 23:37:48 +0100

    Initial Commit

---
+++ When/When.h
@@ -0,0 +1,19 @@
+//
+//  When.h
+//  When
+//
+//  Created by Pawel Szymanski on 13/03/2020.
+//  Copyright © 2020 Paweł Szymański. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for When.
+FOUNDATION_EXPORT double WhenVersionNumber;
+
+//! Project version string for When.
+FOUNDATION_EXPORT const unsigned char WhenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <When/PublicHeader.h>
+
+
